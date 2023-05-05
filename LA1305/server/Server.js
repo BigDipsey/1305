1e0b49d565496b20b7f00f0ae5e91d038b2ca008
Vincent Robert <t.vincentrobert.inf21@stud.bbbaden.ch>
2023-05-05 08:18:54 +0200

    Backend

--- LA1305/server/Server.js
+++ LA1305/server/Server.js
@@ -1,1 +1,36 @@
-console.log('thfjasfjsdlk')
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const vociSchema = new mongoose.Schema({
+  english: String,
+  german: String,
+  french: String,
+})
+
+const question = mongoose.model('voci', vociSchema)
+
+mongoose.set('strictQuery', true)
+
+mongoose
+  .connect(
+    'mongodb+srv://Elvis:BigDipsey@cluster.rkr89yd.mongodb.net/LA1304(Quiz)?retryWrites=true&w=majorit'
+  )
+  .then(() => {
+    console.log('Connected to database')
+  })
+
+app.get('/questions', (req, res) => {
+  question
+    .find({}, { _id: 0 }, (err, data) => {
+      res.json(data)
+    })
+    .catch((err) => console.log(err))
+})
+
+app.listen(3000, () => {
+  console.log('Server running on Port 3000')
+})
